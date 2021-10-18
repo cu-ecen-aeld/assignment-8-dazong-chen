5c185b477e5d2c4415304d90e02a5a20f0244dd3
dazong chen <sunnysimon1995@gmail.com>
2021-10-17 22:36:09 -0600

    test

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -6,16 +6,16 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 7 git contents
-LDD_VERSION = 0a303d88bc7c8d5e36f8617c3a09fdb4ff7eaf64
+LDD_VERSION = 58381795a35f9e6a8039d04cb2142747f3e859b1
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-LDD_SITE = git@github.com:cu-ecen-aeld/assignments-7-dazong-chen.git
+LDD_SITE = git@github.com:cu-ecen-aeld/assignment-7-dazong-chen.git
 LDD_SITE_METHOD = git
 LDD_GIT_SUBMODULES = YES
 
-LDD_MODULE_SUBDIRS = misc-modules
-LDD_MODULE_SUBDIRS += scull
+LDD_MODULE_SUBDIRS = misc-modules/
+LDD_MODULE_SUBDIRS += scull/
 
 # add misc-modules and scull load and unload scripts
 define LDD_INSTALL_TARGET_CMDS
